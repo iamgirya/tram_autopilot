63c68cafae175ee62638271aa1965b74fce1295c
iamgirya <84345515+iamgirya@users.noreply.github.com>
2024-01-27 23:20:37 +0300

    Use windows_capture  and deprecate win32gui

--- test.py
+++ test.py
@@ -1,41 +1,44 @@
-import win32gui
-import win32con
-import cv2
+from windows_capture import WindowsCapture, Frame, InternalCaptureControl
+from time import sleep, time
 import numpy as np
+import cv2
+
+# Every Error From on_closed and on_frame_arrived Will End Up Here
+capture = WindowsCapture(
+    capture_cursor=False,
+    draw_border=None,
+    monitor_index=None,
+    window_name="TramSim  ",
+)
+
+time1 = time()
+frameg = np.zeros((10, 10, 3), np.uint8)
 
 
-def rgbint2rgbtuple(RGBint):
-    blue = RGBint & 255
-    green = (RGBint >> 8) & 255
-    red = (RGBint >> 16) & 255
+# Called Every Time A New Frame Is Available
+@capture.event
+def on_frame_arrived(frame: Frame, capture_control: InternalCaptureControl):
+    global time1, frameg
+    print("New Frame Arrived" + str(time() - time1))
+    time1 = time()
 
-    return (red, green, blue)
+    # Save The Frame As An Image To The Specified Path
+    frameg = frame.frame_buffer[..., :3]
 
-
-def get_tram_sim_frame():
-    hwnd = win32gui.FindWindow(None, "TramSim  ")
-    rect = win32gui.GetWindowRect(hwnd)
-    title = win32gui.GetWindowText(hwnd)
-    x = rect[0]
-    y = rect[1]
-    w = rect[2] - x
-    h = rect[3] - y
-
-    dc = win32gui.GetWindowDC(hwnd)
-    win32gui.ReleaseDC(hwnd, dc)
-    frame = np.zeros((h, w, 3), np.uint8)
-    for i in range(h // 4):
-        for j in range(w // 4):
-            color = rgbint2rgbtuple(win32gui.GetPixel(dc, j, i))
-            # if color[0] != 0 or color[1] != 0 or color[2] != 0:
-            #     print(1)
-            frame[i][j] = color
-    win32gui.ReleaseDC(hwnd, dc)
-    cv2.imshow("lol", frame)
-    cv2.waitKey(0)
-
-    # эта штука очень медленная и постоянно ломается
+    # Gracefully Stop The Capture Thread
 
 
-# main_loop()
-get_tram_sim_frame()
+# Called When The Capture Item Closes Usually When The Window Closes, Capture
+# Session Will End After This Function Ends
+@capture.event
+def on_closed():
+    print("Capture Session Closed")
+
+
+capture.start_free_threaded()
+while True:
+    cv2.imwrite("image.jpg", frameg)
+    cv2.imshow("lol", frameg)
+    sleep(0.1)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
